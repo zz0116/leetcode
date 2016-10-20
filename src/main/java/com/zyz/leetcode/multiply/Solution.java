fb39b94b64b67cc1ecc02c1bc9298c64559bca5f
Yuanzhuo-Zhang <zyz9255@gmail.com>
2016-10-20 22:45:14 +0800

    UPDATE new ac

---
+++ src/main/java/com/zyz/leetcode/multiply/Solution.java
@@ -0,0 +1,12 @@
+package com.zyz.leetcode.multiply;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/10/20.
+ */
+public class Solution {
+    public String multiply(String num1, String num2) {
+        String res = "";
+
+        return res;
+    }
+}
