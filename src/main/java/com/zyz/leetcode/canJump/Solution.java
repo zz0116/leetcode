dec17b7e11336b26d5819f3c2fb62dabfacebb1a
zz0116 <zyz9255@gmail.com>
2016-10-26 22:43:26 +0800

    UPDATE new ac

--- src/main/java/com/zyz/leetcode/canJump/Solution.java
+++ src/main/java/com/zyz/leetcode/canJump/Solution.java
@@ -6,7 +6,20 @@ package com.zyz.leetcode.canJump;
 public class Solution {
     public boolean canJump(int[] nums) {
         boolean res = false;
-
+        if(nums.length<2) {
+            return true;
+        }
+        int jMax = nums[0];
+        for(int i=1; i<nums.length-1; i++) {
+            if(jMax>0) {
+                jMax = Math.max(jMax-1, nums[i]);
+            }else {
+                break;
+            }
+        }
+        if(jMax>0) {
+            res = true;
+        }
         return res;
     }
 }
---
+++ src/test/java/com/zyz/leetcode/canJump/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.zyz.leetcode.canJump;
+
+import org.junit.Test;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/10/26.
+ */
+public class SolutionTest {
+    @Test
+    public void canJump() throws Exception {
+        Solution solution = new Solution();
+
+//        System.out.println(solution.canJump(new int[]{2,3,1,1,4}));
+//        System.out.println(solution.canJump(new int[]{3,2,1,0,4}));
+//        System.out.println(solution.canJump(new int[]{2,0}));
+//        System.out.println(solution.canJump(new int[]{2,0,0}));
+        System.out.println(solution.canJump(new int[]{0,2,3}));
+//        System.out.println(solution.canJump(new int[]{1,2}));
+//        System.out.println(solution.canJump(new int[]{3,0,8,2,0,0,1}));
+    }
+
+}
