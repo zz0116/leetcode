ae858ec22f00eabfda3caeb3f936ad3c573cf6c9
zz0116 <zyz9255@gmail.com>
2016-11-13 22:17:13 +0800

    UPDATE new ac

--- src/main/java/com/zyz/leetcode/Main.java
+++ src/main/java/com/zyz/leetcode/Main.java
@@ -3,8 +3,10 @@ package com.zyz.leetcode;
 public class Main {
 
     public static void main(String[] args) {
+        ListNode l0 = new ListNode(0);
+        l0.next = new ListNode(1);
+        l0.val = 3;
 
-
-        System.out.println('2' < '1');
+        System.out.println(l0.val);
     }
 }
--- src/main/java/com/zyz/leetcode/reverseBetween/Solution.java
+++ src/main/java/com/zyz/leetcode/reverseBetween/Solution.java
@@ -6,7 +6,31 @@ package com.zyz.leetcode.reverseBetween;
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode res = new ListNode(0);
-
-        return res;
+        res.next = head;
+        ListNode pre = res;
+        ListNode cur = head;
+        for (int i = 1; i < m; i++) {
+            pre = cur;
+            cur = cur.next;
+        }
+        ListNode p1 = null;
+        ListNode p2 = null;
+        if (cur != null) {
+            p1 = cur.next;
+        }
+        if (p1 != null) {
+            p2 = p1.next;
+        }
+        for (int i = m; i < n; i++) {
+            p1.next = cur;
+            cur = p1;
+            p1 = p2;
+            if (p2 != null) {
+                p2 = p2.next;
+            }
+        }
+        pre.next.next = p1;
+        pre.next = cur;
+        return res.next;
     }
 }
