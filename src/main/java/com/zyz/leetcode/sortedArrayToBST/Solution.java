f100cb37577856c15906f705c79a20f5d99fb43c
zz0116 <zyz9255@gmail.com>
2016-12-11 18:01:58 +0800

    Update new ac

--- src/main/java/com/zyz/leetcode/sortedArrayToBST/Solution.java
+++ src/main/java/com/zyz/leetcode/sortedArrayToBST/Solution.java
@@ -5,6 +5,24 @@ package com.zyz.leetcode.sortedArrayToBST;
  */
 public class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
+        int l = nums.length;
+        if (l == 0)
+            return null;
+        TreeNode res = new TreeNode(nums[(l - 1) / 2]);
+        helper(nums, res, 0, l, (l - 1) / 2);
+        return res;
+    }
 
+    private void helper(int[] nums, TreeNode t, int left, int right, int key) {
+        if (left >= right)
+            return;
+        if (left < key) {
+            t.left = new TreeNode(nums[(key + left - 1) / 2]);
+            helper(nums, t.left, left, key, (key + left - 1) / 2);
+        }
+        if (key + 1 < right) {
+            t.right = new TreeNode(nums[(key + right) / 2]);
+            helper(nums, t.right, key + 1, right, (key + right) / 2);
+        }
     }
 }
