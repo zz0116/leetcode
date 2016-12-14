7f1bc9a5d66fbb5794b7ed39cd8591159affed1a
zz0116 <zyz9255@gmail.com>
2016-12-14 22:48:28 +0800

    Update new ac

---
+++ src/main/java/com/zyz/leetcode/hasPathSum/Solution.java
@@ -0,0 +1,16 @@
+package com.zyz.leetcode.hasPathSum;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/14.
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null)
+            return root.val == sum;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
---
+++ src/main/java/com/zyz/leetcode/pathSum/Solution.java
@@ -0,0 +1,12 @@
+package com.zyz.leetcode.pathSum;
+
+import java.util.List;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/14.
+ */
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+
+    }
+}
