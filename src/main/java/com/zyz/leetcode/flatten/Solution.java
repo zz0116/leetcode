0e44499fdb9c5b11a88832ea21cba5ce752ef84d
zz0116 <zyz9255@gmail.com>
2016-12-15 22:22:13 +0800

    Update new ac

---
+++ src/main/java/com/zyz/leetcode/flatten/Solution.java
@@ -0,0 +1,10 @@
+package com.zyz.leetcode.flatten;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/15.
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+
+    }
+}
--- src/main/java/com/zyz/leetcode/pathSum/Solution.java
+++ src/main/java/com/zyz/leetcode/pathSum/Solution.java
@@ -1,5 +1,6 @@
 package com.zyz.leetcode.pathSum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,30 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> item = new ArrayList<>();
+        if (root == null)
+            return res;
+        else
+            item.add(root.val);
+        helper(root, sum - root.val, item, res);
+        return res;
+    }
 
+    private void helper(TreeNode t, int remain, List<Integer> item, List<List<Integer>> res) {
+        if (t.left == null && t.right == null && remain == 0) {
+            res.add(new ArrayList<>(item));
+            return;
+        }
+        if (t.left != null) {
+            item.add(t.left.val);
+            helper(t.left, remain - t.left.val, item, res);
+            item.remove(item.size() - 1);
+        }
+        if (t.right != null) {
+            item.add(t.right.val);
+            helper(t.right, remain - t.right.val, item, res);
+            item.remove(item.size() - 1);
+        }
     }
 }
