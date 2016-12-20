88d1694ce59a406dc36398e9c2f693fd5a01f98b
zz0116 <zyz9255@gmail.com>
2016-12-20 22:21:35 +0800

    Update new ac

---
+++ src/main/java/com/zyz/leetcode/getRow/Solution.java
@@ -0,0 +1,12 @@
+package com.zyz.leetcode.getRow;
+
+import java.util.List;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/20.
+ */
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+
+    }
+}
