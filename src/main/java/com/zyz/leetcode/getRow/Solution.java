bc331f47e031ce8b436149332187dbb21ab4cf58
zz0116 <zyz9255@gmail.com>
2016-12-21 22:38:44 +0800

    Update new ac

--- src/main/java/com/zyz/leetcode/getRow/Solution.java
+++ src/main/java/com/zyz/leetcode/getRow/Solution.java
@@ -1,5 +1,6 @@
 package com.zyz.leetcode.getRow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,22 @@ import java.util.List;
  */
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-
+        List<Integer> pre = new ArrayList<>();
+        pre.add(1);
+        if (rowIndex == 0)
+            return pre;
+        pre.add(1);
+        if (rowIndex == 1)
+            return pre;
+        for (int row = 2; row <= rowIndex; row++) {
+            List<Integer> cur = new ArrayList<>();
+            cur.add(1);
+            for (int i = 1; i < row; i++) {
+                cur.add(pre.get(i - 1) + pre.get(i));
+            }
+            cur.add(1);
+            pre = cur;
+        }
+        return pre;
     }
 }
---
+++ src/main/java/com/zyz/leetcode/minimumTotal/Solution.java
@@ -0,0 +1,12 @@
+package com.zyz.leetcode.minimumTotal;
+
+import java.util.List;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/21.
+ */
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+}
