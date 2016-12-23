6632586d35b9a78508705a21d74552d8a52d9389
zz0116 <zyz9255@gmail.com>
2016-12-23 23:01:34 +0800

    Update new ac

---
+++ src/main/java/com/zyz/leetcode/maxProfit/Solution.java
@@ -0,0 +1,10 @@
+package com.zyz.leetcode.maxProfit;
+
+/**
+ * Created by ZhangYuanzhuo on 2016/12/23.
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+
+    }
+}
--- src/main/java/com/zyz/leetcode/minimumTotal/Solution.java
+++ src/main/java/com/zyz/leetcode/minimumTotal/Solution.java
@@ -7,6 +7,16 @@ import java.util.List;
  */
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-
+        int n = triangle.size();
+        int[] res = new int[n];
+        for (int j = 0; j < n; j++) {
+            res[j] = triangle.get(n - 1).get(j);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                res[j] = Math.min(res[j], res[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return res[0];
     }
 }
