743720b210c22dede5187be117ff07574cb5d599
张远卓 <zhangyuanzhuo@tcld.com.cn>
2019-05-06 17:11:38 +0800

    增加先序遍历数组还原二叉树方法

--- src/main/java/com/zyz/leetcode/TreeNode.java
+++ src/main/java/com/zyz/leetcode/TreeNode.java
@@ -11,4 +11,24 @@ public class TreeNode {
     public TreeNode(int x) {
         val = x;
     }
+
+    /**
+     * 由二叉树的先序遍历数组还原二叉树
+     * @param nums
+     * @return
+     */
+    public static TreeNode createTreeFromArray(Integer[] nums) {
+        return createTreeFromArray(nums, 0);
+    }
+
+    private static TreeNode createTreeFromArray(Integer[] nums, int index) {
+        TreeNode tn = null;
+        Integer num;
+        if (index < nums.length && null != (num = nums[index])) {
+            tn = new TreeNode(num);
+            tn.left = createTreeFromArray(nums, index * 2 + 1);
+            tn.right = createTreeFromArray(nums, index * 2 + 2);
+        }
+        return tn;
+    }
 }
